1b9114a5a14d86898a4eac795f01e5f87d3ead36
Mohd Ziyad <mziyadc@gmail.com>
2022-12-21 00:52:42 +0530

    NFT Marketplace unit tests

--- test/unit/nftMarketplace.test.js
+++ test/unit/nftMarketplace.test.js
@@ -19,6 +19,7 @@ const { developmentChains } = require("../../helper-hardhat-config");
               basicNft = await ethers.getContract("BasicNft");
               //   basicNft = await basicNftContract.connect(user);
               await basicNft.mintNft();
+              //approving for marketplace
               await basicNft.approve(nftMarketplace.address, TOKEN_ID);
           });
 
@@ -36,5 +37,134 @@ const { developmentChains } = require("../../helper-hardhat-config");
                   deployer.address
               );
               assert.equal(deployerProceeds.toString(), PRICE.toString());
+          });
+
+          it("exclusively allow owners to list the item", async () => {
+              const userConnectedMarketplace = await nftMarketplace.connect(
+                  user
+              );
+              await expect(
+                  userConnectedMarketplace.listItem(
+                      basicNft.address,
+                      TOKEN_ID,
+                      PRICE
+                  )
+              ).to.be.revertedWith("NftMarketplace__NotOwner");
+          });
+
+          it("allows only approved NFT to be listed", async () => {
+              await basicNft.approve(ethers.constants.AddressZero, TOKEN_ID);
+              await expect(
+                  nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
+              ).to.be.revertedWith("NftMarketplace__NotApprovedForMarketplace");
+          });
+
+          it("checks if the item is not listed", async () => {
+              await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE);
+              await expect(
+                  nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE)
+              ).to.be.revertedWith("NftMarketplace__AlreadyListed");
+          });
+
+          it("checks if the price is valid", async () => {
+              await expect(
+                  nftMarketplace.listItem(
+                      basicNft.address,
+                      TOKEN_ID,
+                      ethers.utils.parseEther("0")
+                  )
+              ).to.be.revertedWith("NftMarketplace__PriceMustbeAboveZero");
+          });
+
+          it("cancels the listing of an item", async () => {
+              await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE);
+              await expect(
+                  nftMarketplace.cancelListing(basicNft.address, TOKEN_ID)
+              ).to.emit(nftMarketplace, "ItemCancelled");
+
+              const listing = await nftMarketplace.getListing(
+                  basicNft.address,
+                  TOKEN_ID
+              );
+              assert(listing.price.toString() == "0");
+          });
+
+          it("updates the current listing", async () => {
+              await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE);
+              const newPrice = ethers.utils.parseEther("0.2");
+              await nftMarketplace.updateListing(
+                  basicNft.address,
+                  TOKEN_ID,
+                  newPrice
+              );
+              const updatedListing = await nftMarketplace.getListing(
+                  basicNft.address,
+                  TOKEN_ID
+              );
+              assert.equal(
+                  updatedListing.price.toString(),
+                  newPrice.toString()
+              );
+          });
+
+          it("allows to withdraw proceeds", async () => {
+              await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE);
+              //   proceeds = await nftMarketplace.getProceeds(deployer.address);
+              await expect(
+                  nftMarketplace.withdrawProceeds()
+              ).to.be.revertedWith("NftMarketplace__NoProceeds");
+
+              const userConnectedMarketplace = await nftMarketplace.connect(
+                  user
+              );
+              await userConnectedMarketplace.buyItem(
+                  basicNft.address,
+                  TOKEN_ID,
+                  { value: PRICE }
+              );
+              const deployerProceedsBefore = await nftMarketplace.getProceeds(
+                  deployer.address
+              );
+              const deployerBalanceBefore = await deployer.getBalance();
+              const txResponse = await nftMarketplace.withdrawProceeds();
+              const txReciept = await txResponse.wait(1);
+              const { gasUsed, effectiveGasPrice } = txReciept;
+              const gasCost = gasUsed.mul(effectiveGasPrice);
+              const deployerBalanceAfter = await deployer.getBalance();
+
+              assert.equal(
+                  deployerBalanceAfter.add(gasCost).toString(),
+                  deployerProceedsBefore.add(deployerBalanceBefore).toString()
+              );
+          });
+
+          it("checks for already listed and reverts if price is low", async () => {
+              await expect(
+                  nftMarketplace.buyItem(basicNft.address, TOKEN_ID, {
+                      value: PRICE,
+                  })
+              ).to.be.revertedWith("NftMarketplace__NotListed");
+
+              await nftMarketplace.listItem(basicNft.address, TOKEN_ID, PRICE);
+              await expect(
+                  nftMarketplace.buyItem(basicNft.address, TOKEN_ID, {
+                      value: ethers.utils.parseEther("0.01"),
+                  })
+              ).to.be.revertedWith("NftMarketplace__NotEnoughETH");
+          });
+
+          describe("Basic NFT unit tests", () => {
+              it("verifies the token URI", async () => {
+                  const tokenUri =
+                      "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+                  const nftTokenUri = await basicNft.tokenURI(0);
+                  assert.equal(tokenUri, nftTokenUri.toString());
+              });
+
+              it("gets the token counter", async () => {
+                  //Nft is already minted. Check above
+                  const tokenCounter = await basicNft.getTokenCounter();
+                  assert.equal(tokenCounter.toString(), "1");
+              });
           });
       });
